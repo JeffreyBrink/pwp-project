ad5c0d4fc629401d7445fca535bc2831272337bf
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-01-21 16:42:32 -0700

    added milestone 2 file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Personal Website Project milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<h3>My site will be one page, and will consist of the following sections:</h3>
+		<br>
+		<h2>Welcome:</h2>
+		A brief one line introduction that states my role as a Front End designer and developer.<br>
+		This message will be overlaid on a background to be determined.
+	<br>
+		<h2>About Me</h2>
+		This section will detail my professional history, core skill set, and professional highlights.<br>
+		This section will showcase my strengths and personality. I will feature a contrasting sidebar<br>
+		that lists my core skills and programming languages which will be easily readable at a glance.<br>
+		The tone of voice will be personable and casual, as I am only interested in opportunities with<br>
+		individuals and organizations that I am a good personality fit for.
+		<br>
+		<h2>Portfolio</h2>
+		I want this to be an animated, responsive image gallery containing 4-6 examples of my best work.<br>
+		Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.<br>
+		<br>
+		<h2>Contact</h2>
+		This section will contain a simple contact form, my professional email address, <br>
+		and additional links to my professional social media profiles:<br>
+		LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)
+	</body>
+
+
+</html>
