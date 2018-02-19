e8643e07cc4505b805c673743ab7abe12017d2e1
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-19 16:45:02 -0700

    saving for the ride home

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
+
 		<ul class="nav justify-content-end">
 			<li class="nav-item">
 				<a class="nav-link active" href="#">Active</a>
@@ -26,11 +27,13 @@
 
 		<title>Jeffrey Brink Personal Website Project</title>
 
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
 
 	</head>
 	<body>
 
-		<!--Navbar-->
+
 		<body >
 
 			<div class="card">
