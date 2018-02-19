f228f802a7fdda373fdb7d1bb63a43039a72ff1d
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-19 10:05:41 -0700

    added css folder and fancybox files

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,3 +1,55 @@
+<!doctype html>
+<html lang="en">
 
+	<head>
+
+	</head>
+
+	<body>
+		<h1>something clever</h1>
+		<h2>city</h2>
+		<div class="thumbnail">
+			<img src="city.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city-1.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city-2.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city-3.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="break"><h2>more cities</h2>
+		</div>
+
+		<div class="float:left">
+			<div class="thumbnail">
+			<img src="city-3.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city-2.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city-1.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		<div class="thumbnail">
+			<img src="city.jpg" alt="" width="60" height="60"><br>
+			Caption
+		</div>
+		</div>
+
+
+	</body>
+
+
+</html>
 
 
