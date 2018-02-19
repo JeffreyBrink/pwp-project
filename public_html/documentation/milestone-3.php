eca459001abdd080b275c44419a8973e419e3c76
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-18 17:26:48 -0700

    added jpg image

---
+++ public_html/documentation/milestone-3.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<title>Hello, world!</title>
+
+	</head>
+	<body>
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scaled=1, shrink-to-fit=no">
+			<script defer src="/static/fontawesome/fontawesome-all.js"></script>
+			<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+			<link rel="stylesheet" type="text/css" href="style/style.css">
+			<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+			<title>Bootstrap Challenge</title>
+		</head>
+		<!--Navbar-->
+		<body >
+
+			<!--Jumbotron-->
+			<div class="container"
+
+
+			<!--  Video is muted & autoplays, placed after major DOM elements for performance & has an image fallback  -->
+			<video autoplay loop id="video-background" muted plays-inline>
+				<source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4">
+			</video>
+			</div>
+			<h1 class="display-3"><strong>Hello, world!</strong></h1>
+			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+			<p class="lead">
+				<a class="btn btn-primary btn-md" href="#" role="button">Learn more</a>
+			</p>
+
+			<!--Image with text inline-->
+
+			<div class="container-fluid">
+				<div class="media d-flex flex-column flex-sm-row">
+					<img class="img-fluid align-self-center p-5" src="03.jpg" alt="cat-1">
+
+					<div class="media-body py-5">
+						<p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					</div>
+
+				</div>
+			</div>
+			<!--4 images with text boxes underneath each image-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<figure class="figure m-4 d-flex-inline">
+							<img src="05.jpg" class="figure-img img-fluid rounded-circle" alt="kitty-2">
+
+							<figcaption class="figure-caption text-center d-sm-inline-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque enim tortor, rhoncus in porta vitae, luctus sit amet nisl.</figcaption>
+
+						</figure>
+					</div>
+					<div class="col-sm-3">
+						<figure class="figure m-4 d-flex-inline">
+							<img src="06.jpg" class="figure-img img-fluid rounded-circle" alt="kitty2">
+							<figcaption class="figure-caption text-center d-sm-inline-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque enim tortor, rhoncus in porta vitae, luctus sit amet nisl.</figcaption>
+						</figure>
+					</div>
+					<div class="col-sm-3">
+						<figure class="figure m-4 d-flex-inline">
+							<img src="05.jpg" class="figure-img img-fluid rounded-circle" alt="kitty-3">
+							<figcaption class="figure-caption text-center d-sm-inline-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque enim tortor, rhoncus in porta vitae, luctus sit amet nisl. </figcaption>
+						</figure>
+					</div>
+					<div class="col-sm-3">
+						<figure class="figure m-4 d-flex-inline">
+							<img src="06.jpg" class="figure-img img-fluid rounded-circle" alt="kitty-4">
+							<figcaption class="figure-caption text-center d-sm-inline-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque enim tortor, rhoncus in porta vitae, luctus sit amet nisl. </figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+			<footer>
+				<div class="card-footer bg-dark text-muted text-center p-4 mt-4">
+					<p>blah blah blah</p>
+				</div>
+			</footer>
+
+		</body>
