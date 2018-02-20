673262f634474dd7fa3d938a240715390e0d9022
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-20 16:50:31 -0700

    saving for the day

--- public_html/documentation/index.php
+++ public_html/documentation/index.php
@@ -1,11 +1,90 @@
 <!doctype html>
 <html lang="en">
 	<meta charset="utf-8">
+	<!-- sets default bootstrap viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<!-- fancyBox CSS -->
+	<link rel="stylesheet" type="text/css"
+			href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css">
+
+	<!-- Personal CSS -->
+	<link rel="stylesheet" href="../css/stylesheet.css"/>
+
+	<!-- jQuery first, then Popper.js, then bootstrap JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+			  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+			  crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+			  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+			  crossorigin="anonymous"></script>
+	<!-- Fancy Box JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
+	<!--		<script src="jquery.fancybox.min.js"></script>-->
+	<!--		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>-->
+
 	<head>
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
-		<script src="jquery.fancybox.min.js"></script>
+
 	</head>
+
+	<body>
+		<div class="jumbotron jumbotron-fluid">
+
+		</div>
+		<h1 align="center">Professional Portfolio</h1>
+		<div class="container">
+
+			<div class="row">
+				<div class="col-md-3 col-sm-6 text-center">
+					<a href="city.jpg" data-fancybox="gallery-city">
+						<img class="img-thumbnail" src="city-1.jpg" alt="city 1"/>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center">
+					<a href="city-2.jpg" data-fancybox="gallery-city" data-caption="city 2">
+						<img class="img-thumbnail" src="city-3.jpg" alt="city-3"/>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center">
+					<a href="city.jpg" data-fancybox="gallery-city" data-caption="another city">
+						<img class="img-thumbnail" src="city-1.jpg" alt="another city"/>
+					</a>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center">
+					<a href="city-2.jpg" data-fancybox="gallery-city" data-caption="another another city">
+						<img class="img-thumbnail" src="city-3.jpg" alt="city yet again"/>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3 col-sm-6 text-center">
+				<a href="city.jpg" data-fancybox="gallery-city">
+					<img class="img-thumbnail" src="city-1.jpg" alt="city 1"/>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<a href="city-2.jpg" data-fancybox="gallery-city" data-caption="city 2">
+					<img class="img-thumbnail" src="city-3.jpg" alt="city-3"/>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<a href="city.jpg" data-fancybox="gallery-city" data-caption="another city">
+					<img class="img-thumbnail" src="city-1.jpg" alt="another city"/>
+				</a>
+			</div>
+			<div class="col-md-3 col-sm-6 text-center">
+				<a href="city-2.jpg" data-fancybox="gallery-city" data-caption="another another city">
+					<img class="img-thumbnail" src="city-3.jpg" alt="city yet again"/>
+				</a>
+			</div>
+		</div>
+
+
 
 
 	</body>
