b25b06b6907f56a1def1027ca410bb6864616929
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-05 22:10:55 -0700

    saving for the night

--- public_html/index.php
+++ public_html/index.php
@@ -65,7 +65,7 @@
 							<a class="nav-link js-scroll-trigger" href="#about">who I am</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link js-scroll-trigger" href="#download">What I do</a>
+							<a class="nav-link js-scroll-trigger" href="#portfolio">What I do</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link js-scroll-trigger" href="#contact-form">Contact me</a>
@@ -75,22 +75,21 @@
 			</div>
 		</nav>
 
-		<section id="masthead" class="content-section text-center">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-8 mx-auto">
-						<h2>About Grayscale</h2>
-						<p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-							<a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-						<p>This theme features stock photos by
-							<a href="http://gratisography.com/">Gratisography</a>
-							along with a custom Google Maps skin courtesy of
-							<a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-						<p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
+		<header class="masthead">
+			<div id="about" class="jumbotron">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 mx-auto">
+							<h1 class="brand-heading">Brink Media Solutions</h1>
+							<p class="intro-text">Creating original content in a media driven world</p>
+							<a href="#about" class="btn btn-circle js-scroll-trigger">
+								<i class="fa fa-angle-double-down animated"></i>
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</>
+		</header>
 
 		<div class="stars">
 		</div>
@@ -100,56 +99,6 @@
 
 
 
-		<!-- Who I am Section -->
-		<!--<section id="download" class="content-section text-center">
-
-				<div class="row">
-					<div class="col">
-						<h2>These are the tools I use to create</h2>
-
-
-							<ul class="list-inline">
-							<li>HTML 5</li>
-
-							<li>PHP</li>
-
-							<li>ANGULAR</li>
-
-							<li>CSS 3</li>
-
-							<li>MySQL</li>
-
-							<li>JAVASCRIPT</li>
-
-							<li>GIT</li>
-
-							<li>GITHUB</li>
-
-						</ul>
-
-						<ul class="list-inline-2">
-							<li>PHOTOSHOP</li>
-
-							<li>AFTER EFFECTS</li>
-
-							<li>ILLUSTRATOR</li>
-
-							<li>PREMIERE</li>
-
-							<li>CINEMA 4D</li>
-
-							<li>NUKE</li>
-
-							<li>FUSION</li>
-
-							<li>HOUDINI</li>
-
-						</ul>
-
-					</div>
-				</div>
-			</div>
-		</section> -->
 		<div class="foreground"></div>
 		<div class="midground">
 			<div class="tuna"></div>
@@ -157,8 +106,10 @@
 		<div></div>
 		<div class="background-2">
 		</div>
-		<section id="download" class="download-section content-section text-center">
-			<div class="jumbotron">
+
+
+		<section id="portfolio" class="download-section content-section text-center">
+			<div id="jumbotron-2" class="jumbotron">
 				<div class="container-full-bg">
 					<h1>Here are a few samples of my work</h1>
 					<h3>click an image to view in slideshow</h3>
