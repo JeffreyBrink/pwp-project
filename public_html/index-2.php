13c5b3f436f45ca47d296b5e8014ed5032d92620
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-05 20:09:07 -0700

    working on css

---
+++ public_html/index-2.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="/css/stylesheet.css" href="../css/style.css"/>
+		<title>Basic HTML Page</title>
+	</head>
+	<div id="tuna"></div>
+</html>
