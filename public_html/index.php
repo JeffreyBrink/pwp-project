9c74ffde3836c95af0577bdc1e7e64638233e3d0
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-05 21:22:11 -0700

    changes to contact page

--- public_html/index.php
+++ public_html/index.php
@@ -75,25 +75,22 @@
 			</div>
 		</nav>
 
-
-
-		<header class="masthead">
-
-							<h6 class="brand-heading">Hi, I am Jeffrey</h6><br>
-							<p class="intro-text"><h4>I am an Albuquerque based
-								Multimedia Creation Artist<br>
-								<br>Specializing in web design<br>
-								Click below to learn more</h4><br>
-							<br>
-							<a href="#download" class="btn btn-circle js-scroll-trigger">
-								<i class="fa fa-angle-double-down animated"></i>
-							</a>
-						</div>
+		<section id="masthead" class="content-section text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 mx-auto">
+						<h2>About Grayscale</h2>
+						<p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+							<a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+						<p>This theme features stock photos by
+							<a href="http://gratisography.com/">Gratisography</a>
+							along with a custom Google Maps skin courtesy of
+							<a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
+						<p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
 					</div>
 				</div>
 			</div>
-			</section>
-		</header>
+		</section>
 
 		<div class="stars">
 		</div>
