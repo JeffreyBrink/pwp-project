b93ba94ff4de9811025d704ba984e3fc649bd573
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-07 14:01:40 -0700

    contact form is fixed.  thanks rochelle

--- public_html/index.php
+++ public_html/index.php
@@ -1,50 +1,52 @@
 <!doctype html>
-
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-	<meta charset="utf-8">
-	<!-- sets default bootstrap viewport -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<html lang="en">
 	<head>
 
+		<meta charset="utf-8">
+		<!-- sets default bootstrap viewport -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 		<title>Jeffrey Brink Personal website project</title>
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<!-- fancyBox CSS -->
-	<link rel="stylesheet" type="text/css"
-			href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css">
 
-	<!-- Personal CS -->
-	<link rel="stylesheet" href="css/stylesheet.css"/>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!-- fancyBox CSS -->
+		<link rel="stylesheet" type="text/css"
+				href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.css">
 
-	<!-- jQuery first, then Popper.js, then bootstrap JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-			  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-			  crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-			  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-			  crossorigin="anonymous"></script> -->
-	<!-- Fancy Box JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
-	<!--		<script src="jquery.fancybox.min.js"></script>-->
-	<!--		<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>-->
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+				type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
+				type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
 
+		<!-- Personal CS -->
+		<link rel="stylesheet" href="css/stylesheet.css"/>
 
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-	<link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
+		<!-- jQuery first, then Popper.js, then bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Plugin JavaScript -->
+		<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js></script>
+		<!-- Custom scripts for this template -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-grayscale/3.3.7/js/grayscale.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+				  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+				  crossorigin="anonymous"></script>
+		<!-- Fancy Box JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.5/jquery.fancybox.min.js"></script>
 
-
-
-
-		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
 		<!-- JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 
@@ -58,7 +60,9 @@
 		<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 			<div class="container">
 				<a class="navbar-brand js-scroll-trigger" href="#page-top">Need a Website?</a>
-				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+						  data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+						  aria-label="Toggle navigation">
 					Menu
 					<i class="fa fa-bars"></i>
 				</button>
@@ -78,8 +82,23 @@
 			</div>
 		</nav>
 
+
+		<div class="stars">
+		</div>
+		<div class="twinkling"></div>
+		<div class="clouds"></div>
+
+
+		<div class="foreground"></div>
+		<div class="midground">
+			<div class="tuna"></div>
+		</div>
+		<div></div>
+		<div class="background-2">
+		</div>
+
 		<header class="masthead">
-			<div id="about" class="jumbotron">
+			<div id="about">
 				<div class="container">
 					<div class="row">
 						<div class="col-lg-8 mx-auto">
@@ -94,21 +113,6 @@
 			</div>
 		</header>
 
-		<div class="stars">
-		</div>
-		<div class="twinkling"></div>
-		<div class="clouds"></div>
-
-
-
-
-		<div class="foreground"></div>
-		<div class="midground">
-			<div class="tuna"></div>
-		</div>
-		<div></div>
-		<div class="background-2">
-		</div>
 
 
 		<section id="portfolio" class="download-section content-section text-center">
@@ -186,58 +190,56 @@
 		</section>
 		<!-- Contact section with contact form -->
 		<!-- <section id="contact-form" class="contact"> -->
-			<div class="container-fluid py-3">
-				<h1 class="text-light text-center mb-0">CONTACT ME</h1>
-			</div>
-			<div class="container p-4">
-				<div class="row p-4">
-					<div class="col-lg-6">
-						<h2>Do you need Media solutions?</h2>
-						<p>Fill out the message form to drop me a line.<br></p>
-						<address>
-							<strong>Brink Media Solutions</strong><br>
-							6709 Jade Park ave ne<br>
-							Albuquerque, NM 87109<br>
-							(505) 385-9371
-						</address>
-					</div>
-					<div class="col-lg-6">
-						<form id="contact-form" action="php/mailer.php" method="post">
-							<div class="form-group">
-								<label for="contactName">Name <span class="text-danger">*</span></label>
-								<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Name">
-							</div>
-							<div class="form-group">
-								<label for="contactEmail">Email address <span class="text-danger">*</span></label>
-								<input type="email" class="form-control" id="contactEmail" name="contactEmail" aria-describedby="emailHelp" placeholder="Email">
-								<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-							</div>
-							<div class="form-group">
-								<label for="contactSubject">Subject <span class="text-danger">*</span></label>
-								<input type="text" class="form-control" id="contactSubject" name="contactSubject" placeholder="Subject">
-							</div>
-							<div class="form-group">
-								<label for="contactMessage">Message <span class="text-danger">*</span></label>
-								<textarea class="form-control" rows="5" id="contactMessage" name="contactMessage" placeholder="Message"></textarea>
-							</div>
-							<!-- reCAPTCHA -->
-							<div class="g-recaptcha" data-sitekey="6LfO_0cUAAAAAOfEW9M1_gXV5-dlSOrdUjBUpz9y"></div>
-							<button class="btn btn-success" type="submit">Send</button>
-						</form>
-						<!--empty area for form error/success output-->
-						<div class="row">
-							<div class="col-xs-12">
-								<div id="output-area"></div>
-							</div>
+		<div class="container-fluid py-3">
+			<h1 class="text-light text-center mb-0">CONTACT ME</h1>
+		</div>
+		<div class="container p-4">
+			<div class="row p-4">
+				<div class="col-lg-6">
+					<h2>Do you need Media solutions?</h2>
+					<p>Fill out the message form to drop me a line.<br></p>
+					<address>
+						<strong>Brink Media Solutions</strong><br>
+						6709 Jade Park ave ne<br>
+						Albuquerque, NM 87109<br>
+						(505) 385-9371
+					</address>
+				</div>
+				<div class="col-lg-6">
+					<form id="contact-form" action="php/mailer.php" method="post" novalidate>
+						<div class="form-group">
+							<label for="contactName">Name <span class="text-danger">*</span></label>
+							<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<label for="contactEmail">Email address <span class="text-danger">*</span></label>
+							<input type="email" class="form-control" id="contactEmail" name="contactEmail"
+									 aria-describedby="emailHelp" placeholder="Email">
+							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.
+							</small>
+						</div>
+						<div class="form-group">
+							<label for="contactSubject">Subject <span class="text-danger">*</span></label>
+							<input type="text" class="form-control" id="contactSubject" name="contactSubject"
+									 placeholder="Subject">
+						</div>
+						<div class="form-group">
+							<label for="contactMessage">Message <span class="text-danger">*</span></label>
+							<textarea class="form-control" rows="5" id="contactMessage" name="contactMessage"
+										 placeholder="Message"></textarea>
+						</div>
+						<!-- reCAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="6LfO_0cUAAAAAOfEW9M1_gXV5-dlSOrdUjBUpz9y"></div>
+						<button class="btn btn-success" type="submit">Send</button>
+					</form>
+					<!--empty area for form error/success output-->
+					<div class="row">
+						<div class="col-xs-12">
+							<div id="output-area"></div>
 						</div>
 					</div>
 				</div>
 			</div>
-
-		<!-- Bootstrap core JavaScript -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<!-- Plugin JavaScript -->
-		<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js></script>
-		<!-- Custom scripts for this template -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-grayscale/3.3.7/js/grayscale.min.js"></script>
+		</div>
 	</body>
+</html>
