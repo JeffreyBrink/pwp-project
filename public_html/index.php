cc1fcf514a628e16e9e6d453fa3ca9d644d13686
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-06 21:51:23 -0700

    saving for the night

--- public_html/index.php
+++ public_html/index.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" href="css/stylesheet.css"/>
 
 	<!-- jQuery first, then Popper.js, then bootstrap JS -->
-	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
 			  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
 			  crossorigin="anonymous"></script>
@@ -235,6 +235,6 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<!-- Plugin JavaScript -->
 		<script src=https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js></script>
-
+		<!-- Custom scripts for this template -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/startbootstrap-grayscale/3.3.7/js/grayscale.min.js"></script>
 	</body>
-</html>
